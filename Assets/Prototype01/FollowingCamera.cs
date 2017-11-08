8e716d420058f952ad3d62e3af7b9d20eb204b8f
Rizler <niry33@gmail.com>
2017-11-08 04:43:31 +0200

    Added basic player controller and camera

---
+++ Assets/Prototype01/FollowingCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _target;
+
+    private Vector3 _distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        _distance = transform.position - _target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = _target.transform.position + _distance;
+    }
+}
--- Assets/Prototype01/PlayerController.cs
+++ Assets/Prototype01/PlayerController.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _movementSpeed = 10;
+    [SerializeField]
+    float _rotationSpeed = 20;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody _playerRb;
+
+    // Use this for initialization
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        _playerRb.velocity = new Vector3(_movementSpeed * horizontal, _playerRb.velocity.y, _movementSpeed * vertical);
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(_playerRb.velocity.x, 0, _playerRb.velocity.z));
+            _playerRb.MoveRotation(Quaternion.Slerp(_playerRb.rotation, targetRotation, Time.fixedDeltaTime * _rotationSpeed));
+        }
+    }
 }
