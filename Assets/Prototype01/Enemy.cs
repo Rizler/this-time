1a17c635107f62863db2634fee8a4887e5b64207
Unknown <theanosmic@gmail.com>
2017-11-10 13:59:01 +0200

    Better platforms test scene

--- Assets/Prototype01/Enemy.cs
+++ Assets/Prototype01/Enemy.cs
@@ -8,12 +8,23 @@ public class Enemy : MonoBehaviour {
 	private float _maxHp = 100;
     private float _hp = 100;
 
+    [SerializeField]
+    private Image healthBarImg;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        if (!healthBarImg) healthBarImg = transform.Find("Canvas").transform.Find("HealthBarImage").GetComponent<Image>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             _hp -= 25;
-            Image healthBarImg = transform.Find("Canvas").transform.Find("HealthBarImage").GetComponent<Image>();
             healthBarImg.fillAmount = _hp / _maxHp;
             if (_hp <= 0)
             {
