c2671acef646679860ec67ba78ce2588d4a0cf05
Unknown <theanosmic@gmail.com>
2017-11-11 00:06:23 +0200

    Enemies Attack. sort of. well they try to hug you.

---
+++ Assets/Prototype01/CameraFacingBillboard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
+ 
+    void Update()
+    {
+		if (!m_Camera) m_Camera = Camera.main;
+		
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
--- Assets/Prototype01/Enemy.cs
+++ Assets/Prototype01/Enemy.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour {
 
 	private float _maxHp = 100;
     private float _hp = 100;
+    private NavMeshAgent agent;
 
     [SerializeField]
     private Image healthBarImg;
 
+    [SerializeField]
+    private Transform healthCanvasContainer;
+
+    public Transform playerTransform {get;set;}
+
     void Start()
     {
         if (!healthBarImg) healthBarImg = transform.Find("Canvas").transform.Find("HealthBarImage").GetComponent<Image>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() 
+    {
+        if (playerTransform) {
+            agent.destination = playerTransform.position;
+        }
     }
 
     public void Hit()
